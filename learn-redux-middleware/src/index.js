25ef0b8662f11fa64750943948f132d299f9ba63
youngjun.jin <yzzzzun@gmail.com>
2020-03-25 21:41:02 +0900

    redux-logger middleware 사용하기

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -6,9 +6,11 @@ import * as serviceWorker from "./serviceWorker";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
-import loggerMiddleware from "./lib/loggerMiddleware";
+//import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
