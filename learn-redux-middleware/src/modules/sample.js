02e78506270df7bc3f6fa2d34d21924f87830ad7
youngjun.jin <yzzzzun@gmail.com>
2020-03-25 22:45:23 +0900

    비동기 처리 thunk 함수 리팩토링

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,6 +1,6 @@
 import { handleActions } from "redux-actions";
 import * as api from "../lib/api";
-import { truncate } from "fs";
+import createRequestThunk from "../lib/createRequestThunk";
 
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
@@ -10,45 +10,8 @@ const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
 const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
-export const getPost = id => async dispatch => {
-  dispatch({ type: GET_POST });
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data
-    });
-    //요청성공
-  } catch (e) {
-    //요청실패
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true
-    });
-    throw e;
-  }
-};
-
-export const getUsers = () => async dispatch => {
-  dispatch({ type: GET_USERS });
-  try {
-    const response = await api.getUsers();
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data
-    });
-    //요청성공
-  } catch (e) {
-    //요청실패
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true
-    });
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 const initialState = {
   loading: {
