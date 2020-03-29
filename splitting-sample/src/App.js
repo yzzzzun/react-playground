ab883cd74f5fabd861cf3d6651a543f1b10d1c66
youngjun.jin <yzzzzun@gmail.com>
2020-03-29 18:47:25 +0900

    state 를 사용한 코드 스플리팅

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 // import notify from "./notify";
 
-function App() {
-  const onClick = () => {
-    import("./notify").then(result => result.default());
+class App extends Component {
+  state = {
+    SplitMe: null
   };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-      </header>
-    </div>
-  );
+
+  handleClick = async () => {
+    const loadedModule = await import("./SplitMe");
+    this.setState({
+      SplitMe: loadedModule.default
+    });
+  };
+
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
