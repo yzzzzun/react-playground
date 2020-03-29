d13f48b4eb07bb09b803bff6dd6b122478f4778a
youngjun.jin <yzzzzun@gmail.com>
2020-03-29 18:41:00 +0900

    함수 비동기 로딩

---
+++ splitting-sample/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import notify from "./notify";
+
+function App() {
+  const onClick = () => {
+    import("./notify").then(result => result.default());
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello React!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
