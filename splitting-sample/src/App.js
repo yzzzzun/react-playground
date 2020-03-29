7c731505709acbc0c43f9173edf1cf151e8ef696
youngjun.jin <yzzzzun@gmail.com>
2020-03-29 19:25:39 +0900

    loadable component로 코드 스플리팅 -> SSR 지원이 장점

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,22 +1,29 @@
-import React, { useState, Suspense } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import loadable from "@loadable/component";
 // import notify from "./notify";
 
-const SplitMe = React.lazy(() => import("./SplitMe"));
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>Loading...</div>
+});
+
 const App = () => {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
