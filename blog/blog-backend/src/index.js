8a65ce5b04fcd0f1361bc6089caf73cf1fc6c2c4
youngjun.jin <yzzzzun@gmail.com>
2020-03-30 20:40:05 +0900

    node koa router 모듈화 하기

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,44 +1,15 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const api = require('./api');
+const posts = require('./api/posts');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', ctx => {
-  ctx.body = 'home';
-});
+router.use('/api', api.routes());
+router.use('/api/posts', posts.routes());
 
-router.get('/about/:name', ctx => {
-  const { name } = ctx.params;
-  ctx.body = name ? `about ${name}` : 'about';
-});
-
-router.get('/posts', ctx => {
-  const { id } = ctx.query;
-  ctx.body = id ? `post #${id}]` : 'post id 없음';
-});
 app.use(router.routes()).use(router.allowedMethods());
-
-// app.use(async (ctx, next) => {
-//   console.log(ctx.url);
-//   console.log(1);
-
-//   if (ctx.query.authorized !== '1') {
-//     ctx.status = 401;
-//     return;
-//   }
-//   await next();
-//   console.log('END');
-// });
-
-// app.use((ctx, next) => {
-//   console.log(2);
-//   next();
-// });
-
-// app.use(ctx => {
-//   ctx.body = 'hello world';
-// });
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
